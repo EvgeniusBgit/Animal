f68e050a88d9b3105b5df48a25e07f1e732c6fd8
Bagdasarov_EL <BagdasarovEL@gmail.com>
2020-02-03 10:30:39 +0300

    0

--- Pets/Program.cs
+++ Pets/Program.cs
@@ -34,6 +34,7 @@ namespace Pets
         }
         public string vid = " ";
         public Animals(string n, DateTime b, Sex My) { Name = n; Birthday = b; MySex = My;}
+        public Animals() { }
         public virtual void SayHello()
         {
             
@@ -45,10 +46,8 @@ namespace Pets
 
     public class Zebra : Animals
     {
-        public Zebra(string n, DateTime b, Sex My) : base(n, b, My)
-        {
-        }
-
+        public Zebra(string n, DateTime b, Sex My) : base(n, b, My) { }
+        public Zebra() { }
         public override void SayHello()
         {
             vid = " Зебра ";
@@ -60,9 +59,8 @@ namespace Pets
     public class Elephant : Animals
     {
         public Elephant(string n, DateTime b, Sex My) : base(n, b, My)
-        {
-        }
-
+        { }
+        public Elephant() { }
         public override void SayHello()
         {
             vid = " Слон ";
@@ -74,11 +72,9 @@ namespace Pets
 
     public class Giraffe : Animals
     {
-        public Giraffe(string n, DateTime b, Sex My) : base(n, b, My)
-        {
-        }
-
-         public override void SayHello()
+        public Giraffe(string n, DateTime b, Sex My) : base(n, b, My) { }
+        public Giraffe() { }
+        public override void SayHello()
          {
              
         vid = " Жираф ";
@@ -102,7 +98,104 @@ namespace Pets
 
     class Program
     {
-        static void Main(string[] args)
+        static void ListOfZebra(List<Animals> ts)
+        {
+            foreach (var i in ts)
+            {
+                if (i.GetType() == new Zebra().GetType()) { i.SayHello(); }
+            }
+        }
+        static void ListOfOlder2(List<Animals> ts)
+        {
+            
+            foreach (var i in ts)
+            {
+                if (i.Age>2) { i.SayHello(); }
+            }
+        }
+        static void UniqueNames(List<Animals> ts)
+        {
+            var listNames = new List<string>();
+            int s=0;
+            foreach (var i in ts)
+            {
+                s++;
+                listNames.Add(i.Name);
+            }
+            listNames.Sort();
+            for (int d = 1; d < s ; d++)
+            {
+                for (int i = 1; i < s; i++)
+                {
+                    if (listNames[i] == listNames[i - 1]) { listNames.RemoveAt(i); s--; }
+                }
+            }
+
+            foreach (var i in listNames)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void BirthdayElephant(List<Animals> ts)
+        {
+            foreach (var i in ts)
+            {
+                if (i.GetType() == new Elephant().GetType()) { Console.WriteLine(i.Birthday); }
+            }
+        }
+
+        static void AgeClass(List<Animals> ts)
+        {
+            var listClass = new List<string>();
+            int s = 0;
+            foreach (var i in ts)
+            {
+                s++;
+                listClass.Add(i.GetType().Name);
+            }
+            listClass.Sort();
+            int s1 = s;
+            for (int d = 1; d < s; d++)
+            {
+                for (int i = 1; i < s; i++)
+                {
+                    if (listClass[i] == listClass[i - 1]) { listClass.RemoveAt(i); s--; }
+
+                }
+            }
+            
+            int allage = 0; int sred = 0;
+            for (int d = 0; d < s; d++)
+            {
+                for(int i = 1; i < s1; i++)
+                { 
+                    if (listClass[d] == ts[i].GetType().Name) { allage = allage + ts[i].Age; sred++; }
+                }
+                Console.WriteLine($"Средний возраст {listClass[d]} равен {allage / sred}"); allage = 0; sred = 0;
+            }
+        }
+        static void OldMeat(List<Animals> ts)
+        {
+            
+            int s = 0;
+            foreach (var i in ts)
+            {
+                s++;
+            }
+            Console.WriteLine(s);
+            ts.Add(new Animals());
+            for (int d = 0; d < s; d++)
+            {
+                for (int i = 1; i <= s; i++)
+                {
+                    if (ts[i - 1].Age < ts[i].Age) { ts[s] = ts[i - 1]; ts[i - 1] = ts[i]; ts[i] = ts[s]; }
+                }
+
+            }
+            for (int d = 0; d < 3; d++) { ts[d].SayHello(); }
+        }
+
+            static void Main(string[] args)
         {
             Animals Dog = new Animals("Doofy", new DateTime(2018, 7, 20), Animals.Sex.male);
             Dog.SayHello();
@@ -126,10 +219,32 @@ namespace Pets
                 new Giraffe("Kate", new DateTime(2010, 1, 24), Animals.Sex.Female),
                 new Giraffe("Harry", new DateTime(2008, 3, 09), Animals.Sex.male),
                 new Elephant("Bender", new DateTime(2013, 12, 18), Animals.Sex.male),
+                new Elephant("Bender", new DateTime(2013, 12, 18), Animals.Sex.male),
+                new Animals("Doorry", new DateTime(2018, 7, 21), Animals.Sex.Female),
             };
 
             listOfAnimals.SayHelloList();
-             
+
+            // This version for expansion 
+            Console.WriteLine("Зебры:");
+            ListOfZebra(listOfAnimals);
+
+            Console.WriteLine("Возраст больше 2:");
+            ListOfOlder2(listOfAnimals);
+
+            Console.WriteLine("Уникальные именна животных:");
+            UniqueNames(listOfAnimals);
+            
+            Console.WriteLine("Дни рождения слонов:");
+            BirthdayElephant(listOfAnimals);
+           
+            AgeClass(listOfAnimals);
+            
+            Console.WriteLine("Три самых старых животных");
+            OldMeat(listOfAnimals);
         }
+
+      
+
     }
 }
